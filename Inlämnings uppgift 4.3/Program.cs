8a75c673fb80529ef6fbffe122d50cd1995fb71c
baronvonpurr <robinhood.svensson@hotmail.com>
2023-10-20 12:44:50 +0200

    Add project files.

---
+++ Inlämnings uppgift 4.3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Uppgift4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nummer = 1;
+
+            while (nummer < 101)
+            {
+                if (nummer % 3 == 0 && nummer % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                    nummer++;
+                }
+                else if (nummer % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                    nummer++;
+                }
+                else if (nummer % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                    nummer++;
+                }
+                else
+                {
+                    Console.WriteLine(nummer);
+                    nummer++;
+                }
+            }
+        }
+    }
+}
